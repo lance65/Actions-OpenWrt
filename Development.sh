532b37477e79abed122277a8e776fa5786c3c635
pema1029 <67303241+pema1029@users.noreply.github.com>
2020-08-06 15:45:42 +0800

    Update Development.sh

--- Development.sh
+++ Development.sh
@@ -5,9 +5,19 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 # Author: Microdop
 #=============================================================
-
-#修改默认ip
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#修改机器名称
+sed -i 's/OpenWrt/Microdop/g' package/base-files/files/bin/config_generate
+#修改网关地址
+sed -i 's/192.168.1.1/10.10.10.130/g' package/base-files/files/bin/config_generate
+#修改时区
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+#修改默认主题
+sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+#去除默认bootstrap主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#删除旧版argon,链接新版
+rm -rf ./package/lean/luci-theme-argon
+ln -s ../../../luci-theme-argon ./package/lean/
 #修改版本号
 sed -i 's/OpenWrt/Microdop Development AutoBuild $(TZ=UTC-8 date "+%Y%m%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
 
